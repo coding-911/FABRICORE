03cf0279040d6859ee3e4697a77df3aeaba89bfb
coding911 <ye22im@gmail.com>
2026-03-12 16:40:36 +0900

    init

---
+++ src/test/java/com/bapful/fabricore/FabricoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bapful.fabricore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
