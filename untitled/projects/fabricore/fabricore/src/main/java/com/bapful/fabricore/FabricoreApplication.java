25e74ac82d25b0bea2e821436ac6525829edbf15
coding911 <ye22im@gmail.com>
2026-03-12 16:24:40 +0900

    init

---
+++ untitled/projects/fabricore/fabricore/src/main/java/com/bapful/fabricore/FabricoreApplication.java
@@ -0,0 +1,13 @@
+package com.bapful.fabricore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricoreApplication.class, args);
+	}
+
+}
